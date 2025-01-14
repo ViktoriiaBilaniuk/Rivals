b2091d0c2110c18fb2e282a18387c12f0a37ac4a
Vadim Parpolita <vadim.parpolita10@gmail.com>
2025-01-14 14:30:08 +0200

    Added routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app'
+import {Route, Routes} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
